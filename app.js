43bcbc4684a65b27f5790738105fcf284353f628
gh546 <gh546@DOFMPC469.its.york.ac.uk>
2013-07-08 16:21:31 +0100

    change of names

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ window.addEventListener('load', function () {
 // The dynamically built HTML pages. In a real-life app, In a real-life app, use Handlerbar.js, Mustache.js or another template engine
 var homePage =
     '<div>' +
-        '<div class="header"><h1>Page Slider</h1></div>' +
+        '<div class="header"><h1>Home</h1></div>' +
         '<div class="scroller">' +
             '<ul class="list">' +
-                '<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
-                '<li><a href="#page2"><strong>Medi Bot</strong></a></li>' +
-                '<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
+                '<li><a href="#page1"><strong>Cafe 1</strong></a></li>' +
+                '<li><a href="#page2"><strong>Cafe 2</strong></a></li>' +
+                '<li><a href="#page3"><strong>Cafe 3</strong></a></li>' +
             '</ul>' +
         '</div>' +
     '</div>';
 
 var detailsPage =
     '<div>' +
-        '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+        '<div class="header"><a href="#" class="btn">Home</a><h1>Robot</h1></div>' +
         '<div class="scroller">' +
             '<div class="robot">' +
                 '<img src="images/{{img}}"/>' +
@@ -37,13 +37,13 @@ function route(event) {
         hash = window.location.hash;
 
     if (hash === "#page1") {
-        page = merge(detailsPage, {img: "buildbot.jpg", name: "Build Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+        page = merge(detailsPage, {img: "buildbot.jpg", name: "Cafe 1", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
 //        slider.slide($(page), "right");
     } else if (hash === "#page2") {
-        page = merge(detailsPage, {img: "medibot.jpg", name: "Medi Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+        page = merge(detailsPage, {img: "medibot.jpg", name: "Cafe 2", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
 //        slider.slide($(page), "right");
     } else if (hash === "#page3") {
-        page = merge(detailsPage, {img: "ripplebot.jpg", name: "Ripple Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+        page = merge(detailsPage, {img: "ripplebot.jpg", name: "Cafe 3", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
 //        slider.slide($(page), "right");
     }
     else {
